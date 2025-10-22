78cb5b2031b1c9c6a5ad01c950626fe333aaea2f
HomeGrowLocal <ottiger.silvan@gmail.com>
2025-10-22 11:47:01 +0200

    Add files via upload

--- update/update.js
+++ update/update.js
@@ -1,12 +1,14 @@
-const fs = require('fs');
-const path = require('path');
-const adac = require('./sources/adac');
-const park4night = require('./sources/park4night');
-const trustpilot = require('./sources/trustpilot');
-const campinginfo = require('./sources/campinginfo');
-(async () => {
-  const camping = await adac.getData();
-  const stell = await park4night.getData();
-  fs.writeFileSync(path.join(__dirname, '../data/campingplaetze.json'), JSON.stringify(camping, null, 2));
-  fs.writeFileSync(path.join(__dirname, '../data/stellplaetze.json'), JSON.stringify(stell, null, 2));
-})();
+
+const { exec } = require('child_process');
+
+console.log('Starte Update...');
+
+exec('node sources/park4night.js', (err, stdout, stderr) => {
+  if (err) console.error(stderr);
+  else console.log(stdout);
+});
+
+exec('node sources/adac.js', (err, stdout, stderr) => {
+  if (err) console.error(stderr);
+  else console.log(stdout);
+});
