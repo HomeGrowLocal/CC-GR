8bd2c242d1350165a251e02c2d881d207f261f5e
HomeGrowLocal <ottiger.silvan@gmail.com>
2025-10-22 09:40:33 +0200

    Add files via upload

--- update/update.js
+++ update/update.js
@@ -1,3 +1,12 @@
 const fs = require('fs');
-// Hier würden Scraper für ADAC, Park4Night etc. implementiert werden
-console.log('Update-Skript ausgeführt');
+const path = require('path');
+const adac = require('./sources/adac');
+const park4night = require('./sources/park4night');
+const trustpilot = require('./sources/trustpilot');
+const campinginfo = require('./sources/campinginfo');
+(async () => {
+  const camping = await adac.getData();
+  const stell = await park4night.getData();
+  fs.writeFileSync(path.join(__dirname, '../data/campingplaetze.json'), JSON.stringify(camping, null, 2));
+  fs.writeFileSync(path.join(__dirname, '../data/stellplaetze.json'), JSON.stringify(stell, null, 2));
+})();
